8a779d0f6a56a661289c08397112867a7b6bb4f5
github username <github email address>
2016-10-12 14:55:12 -0400

    Spaghetti

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,5 +1,25 @@
 require_relative 'stack'
 
 class Queue
+  attr_accessor :stack1, :stack2
+
+  def initialize
+    @stack1 = Stack.new
+
+  end
+
+
+  def enqueue(num)
+    @stack1.push(num)
+    @stack2 = @stack1.list.reverse
+  end
+
+  def dequeue
+    new_boy = @stack2.pop
+    @stack1.list = stack2.reverse
+    new_boy
+  end
+
+
 
 end
--- lib/stack.rb
+++ lib/stack.rb
@@ -1,3 +1,19 @@
 class Stack
+  attr_accessor :list
 
+  def initialize
+    @list = []
+  end
+
+  def push(num)
+    @list << num
+  end
+
+  def pop
+    @list.pop
+  end
+
+  def size
+    @list.length
+  end
 end
